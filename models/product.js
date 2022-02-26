c1e8ab955ee7150787148570df382c1405ed245b
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-26 14:30:36 +0800

    model product

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const conn = require("../../middleware/connection");
+
+const modelDesign = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        slug: { type: String, required: true, unique: true },
+        category: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        brand: { type: String, required: true },
+        rating: { type: Number, required: true, default: 0 },
+        numReviews: { type: Number, required: true, default: 0 },
+        countInStock: { type: Number, required: true, default: 0 },
+        description: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = conn.dropgodb.model('Product', productSchema);
