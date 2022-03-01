fb0c083f8298c119cadb16e05711dba1893e8c6c
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-03-01 14:54:59 +0800

    admin

--- graphql/admin-controller/dashboard-summary.js
+++ graphql/admin-controller/dashboard-summary.js
@@ -3,11 +3,17 @@ const Product = require("../../models/product")
 const User = require("../../models/user")
 
 exports.dashboardSummarySchema = `
+type salesDataRes {
+    _id: String!,
+    totalSales: Float!,
+}
 
 type dashboardSummaryRes {
     ordersCount: Int!,
     productsCount: Int!,
     usersCount: Int!,
+    ordersPrice: Float!,
+    salesData: [salesDataRes!],
 }
 
 extend type Query {
@@ -23,17 +29,60 @@ exports.dashboardSummaryController = {
             errorName
         }) => {
             try {
-                if (!req._id || req.isAdmin) {
+                if (!req._id || !req.isAdmin) {
                     throw new Error(errorName.UNAUTHORIZED);
                 }
+
                 const ordersCount = await Order.count({})
                 const productsCount = await Product.count({})
                 const usersCount = await User.count({})
+                let ordersPrice = 0.0;
+                let salesData = [];
+                orders = await Order.find({}, { totalPrice: 1, createdAt: 1 })
+
+                let ordersPriceTempt = 0.0;
+                orders.map((order) => {
+
+                    ordersPriceTempt = ordersPriceTempt + order.totalPrice;
+                })
+                ordersPrice = ordersPriceTempt
+
+
+                orders.map((order) => {
+                    const options = { year: 'numeric', month: 'numeric' };
+                    _id = new Date(order.createdAt).toLocaleDateString('en-EN', options)
+
+                    if (salesData) {
+                        let salesDataTrue = false;
+                        salesData.forEach((salesDataOne) => {
+                            if (salesDataOne._id == _id) {
+                                salesDataTrue = true;
+                            }
+                        });
+                        if (salesDataTrue) {
+                            salesData[salesData.findIndex((salesDataOne => salesDataOne._id == _id))].totalSales = salesData[salesData.findIndex((salesDataOne => salesDataOne._id == _id))].totalSales + order.totalPrice
+                        } else {
+                            salesData.push({
+                                _id: _id,
+                                totalSales: order.totalPrice
+                            })
+                        }
+                    } else {
+                        salesData.push({
+                            _id: _id,
+                            totalSales: order.totalPrice
+                        })
+                    }
+                })
+                console.log(salesData)
+
 
                 return {
                     ordersCount: ordersCount,
                     productsCount: productsCount,
                     usersCount: usersCount,
+                    ordersPrice: ordersPrice,
+                    salesData: salesData,
                 }
 
             } catch (err) {
